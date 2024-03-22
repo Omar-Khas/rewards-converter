49c01fa85c53e65c5e76ddb205332241f087d701
Khas <alkhasomar@gmail.com>
2024-03-22 08:21:57 +0200

    Adding RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+    private static final double CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / CONVERSION_RATE);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
